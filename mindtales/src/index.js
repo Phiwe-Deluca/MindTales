bdbbf13433158734e4c7d6e1c0547948c2237724
simoloijhb024 <simoloijhb024@student.wethinkcode.co.za>
2025-05-07 13:37:58 +0200

    2nd commit

--- mindtales/src/index.js
+++ mindtales/src/index.js
@@ -11,7 +11,6 @@ import DiaryProvider from './context/DiaryContext';
 // Import global styles (optional)
 import './styles/index.css';
 
-//commit 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
